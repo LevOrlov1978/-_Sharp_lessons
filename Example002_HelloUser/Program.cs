03145606df0f7d792191a9ca32a7ece3c63448ae
email <or_lev@mail.ru>
2022-08-07 13:58:04 +0300

    пример 2

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("введите имя");
+string username = Console.ReadLine();
+Console.Write("Привет,");
+Console.Write(username);
